90113779bd69f41332c7e4d49312029849071ea6
Binh Bui <baobinh.ams1013@gmail.com>
2017-05-25 23:35:31 +0300

    Initial Commit

---
+++ TorrentParser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TorrentParser
+//
+//  Created by Binh Bui on 25/05/2017.
+//  Copyright © 2017 Binh Bui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
